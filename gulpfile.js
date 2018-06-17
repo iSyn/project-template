4284361b909d241823717ca202237f41ff6389ab
Synclair Wang <synclairwang11@yahoo.com>
2018-06-17 14:59:23 -0400

    basic boilerplate

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+let gulp = require('gulp')
+let sass = require('gulp-sass')
+let browserSync = require('browser-sync').create()
+
+gulp.task('browserSync', () => {
+    browserSync.init({
+        server: {
+            baseDir: 'app',
+            // directory: true
+        }
+    })
+})
+
+gulp.task('sass', () => { // create a gulp task called sass
+    return gulp.src('app/scss/**/*.scss')  // tells gulp what files are needed
+        .pipe(sass()) // sends the files through gulp sass
+        .pipe(gulp.dest('app/css')) // destination of files
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+})
+
+// Gulp watch syntax
+// gulp.watch('files-to-watch', ['tasks', 'to', 'run']); 
+
+gulp.task('watch', ['browserSync'], () => {
+    gulp.watch('app/scss/**/*.scss', ['sass'])
+    gulp.watch('app/*.html', browserSync.reload); 
+    gulp.watch('app/js/**/*.js', browserSync.reload); 
+})
