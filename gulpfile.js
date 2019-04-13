cea7aa4a21a0760473c15706fd1535b4830e733e
Synclair Wang <synclairwang11@yahoo.com>
2019-04-13 11:28:36 -0400

    adding dotenv to template

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
+// REQUIRE DOTENV
+// to use, create a .env file
+require('dotenv').config()
+
+
+// DEFINING VARIABLES
 let gulp = require('gulp')
 let sass = require('gulp-sass')
 let plumber = require('gulp-plumber')
 let autoprefixer = require('gulp-autoprefixer')
 let browserSync = require('browser-sync').create()
 
+// TASK FOR BROWSERSYNC
 gulp.task('browserSync', () => {
     browserSync.init({
         server: {
@@ -13,6 +20,7 @@ gulp.task('browserSync', () => {
     })
 })
 
+// TASK FOR SASS
 gulp.task('sass', () => { // create a gulp task called sass
     return gulp.src('app/scss/**/*.scss')  // tells gulp what files are needed
         .pipe(plumber())
@@ -30,6 +38,7 @@ gulp.task('sass', () => { // create a gulp task called sass
 // Gulp watch syntax
 // gulp.watch('files-to-watch', ['tasks', 'to', 'run']); 
 
+// TASK TO START EVERYTHING
 gulp.task('watch', ['browserSync'], () => {
     gulp.watch('app/scss/**/*.scss', ['sass'])
     gulp.watch('app/*.html', browserSync.reload); 
